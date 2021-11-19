316a2607aa8d3fec352177d2d0e2bd97f0825937
Prasoon Pandey <prasoonpandey06@gmail.com>
2021-11-19 23:19:05 +0530

    Create find shop activity
    When the user clicks on the find shop
    button in the main activity, this
    'find shop' activity will start,
    allowing them to find a shop on google
    maps near their location

---
+++ app/src/main/java/com/prasoon/shoppingcartv2/FindShop.java
@@ -0,0 +1,14 @@
+package com.prasoon.shoppingcartv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindShop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_shop);
+    }
+}
--- app/src/main/java/com/prasoon/shoppingcartv2/MainActivity.java
+++ app/src/main/java/com/prasoon/shoppingcartv2/MainActivity.java
@@ -14,8 +14,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Starts the Add Item activity to let the user add any item to their shopping list
+     * @param view
+     */
     public void addItem(View view){
         Intent intent = new Intent(this, AddItem.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Starts the Find Shop activity to help the user find a suitable shop near their location
+     * @param view
+     */
+    public void findShop(View view){
+        Intent intent = new Intent(this, FindShop.class);
         startActivity(intent);
     }
 }
