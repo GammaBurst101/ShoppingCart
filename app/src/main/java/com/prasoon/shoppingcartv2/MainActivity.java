50a312d2032e0ff25a643bca22f2f8571e3bb045
Prasoon Pandey <prasoonpandey06@gmail.com>
2021-11-20 20:18:26 +0530

    Add the entered item onto the linear layout, "list"
    When the user clicks on the 'add item' button
    on the add item activity, we were only
    returning the data before but now, a new
    textview is created in a specific format and
    it is added to the list of items

--- app/src/main/java/com/prasoon/shoppingcartv2/MainActivity.java
+++ app/src/main/java/com/prasoon/shoppingcartv2/MainActivity.java
@@ -10,9 +10,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout listLayout;
 
     //Using the Activity Result API
     //For that, we have to first register a launcher, which will launch the required activity while expecting a result
@@ -22,11 +27,23 @@ public class MainActivity extends AppCompatActivity {
             new ActivityResultCallback<ActivityResult>() {//Anonymous class
                 @Override
                 public void onActivityResult(ActivityResult result) {//Callback method
-                    if(result.getResultCode() == Activity.RESULT_OK){
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        //Get the data which was returned
                         Intent intent = result.getData();
                         Bundle extras = intent.getExtras();
+                        String itemName = extras.getString(AddItem.ITEM_NAME_TAG);
+                        int quantity = extras.getInt(AddItem.QUANTITY_TAG);
 
-                        //Log.d("DEBUG", "\nReturned :- \n 1. Item name = "+extras.getString(AddItem.ITEM_NAME_TAG)+ " \n 2. Quantity = "+extras.getInt(AddItem.QUANTITY_TAG));
+                        //Debug statement
+//                        Log.d("DEBUG", "\nReturned :- \n 1. Item name = "+extras.getString(AddItem.ITEM_NAME_TAG)+ " \n 2. Quantity = "+extras.getInt(AddItem.QUANTITY_TAG));
+
+                        //Create a new text view to display the new item added
+                        if (!itemName.equals("") && quantity > 0) {
+                            TextView newItem = new TextView(getApplicationContext());
+                            newItem.setText(itemName + " -> " + quantity);
+                            newItem.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+                            listLayout.addView(newItem); // Add the newly created item to the linear layout, 'list'
+                        }
                     }
                 }
             });
@@ -35,22 +52,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listLayout = findViewById(R.id.list);
     }
 
     /**
      * Starts the Add Item activity to let the user add any item to their shopping list
+     *
      * @param view
      */
-    public void addItem(View view){
+    public void addItem(View view) {
         Intent intent = new Intent(this, AddItem.class);
         activityLauncher.launch(intent); //Using the recommended Activity Result API for launching another activity
     }
 
     /**
      * Starts the Find Shop activity to help the user find a suitable shop near their location
+     *
      * @param view
      */
-    public void findShop(View view){
+    public void findShop(View view) {
         Intent intent = new Intent(this, FindShop.class);
         startActivity(intent);
     }
