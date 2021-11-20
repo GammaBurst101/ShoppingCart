be1b636ebc42570c0d06b25d8c69358951a4e9fe
Prasoon Pandey <prasoonpandey06@gmail.com>
2021-11-20 19:44:17 +0530

    Return the item & quantity entered by the user back to the main activity

--- app/src/main/java/com/prasoon/shoppingcartv2/AddItem.java
+++ app/src/main/java/com/prasoon/shoppingcartv2/AddItem.java
@@ -2,13 +2,52 @@ package com.prasoon.shoppingcartv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddItem extends AppCompatActivity {
+
+    private EditText nameField, quantityField;
+    public static String ITEM_NAME_TAG = "item name", QUANTITY_TAG = "item quantity"; //These tags are to be used both here and in MainActivity.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        //Assigning the appropriate views to each instance variable
+        nameField = findViewById(R.id.name_et);
+        quantityField = findViewById(R.id.quantity_et);
+    }
+
+    public void addItem(View view){
+        //Get the item name and the quantity to be added to the list
+        String item = ""; //Default value
+        int quantity = -1; //Default value
+
+        //Try getting the name and quantity. If unsuccessful, print the error stack trace
+        try {
+            item = nameField.getText().toString();
+            quantity = Integer.parseInt(quantityField.getText().toString());
+        } catch (Exception e){ //Should use a more specific exception, but I'm too lazy :P
+            e.printStackTrace();
+        }
+
+        //Intent to store everything which has to be sent back
+        Intent resultIntent = new Intent();
+
+        //Bundling all the data inside 'extras'
+        Bundle extras = new Bundle();
+        extras.putString(ITEM_NAME_TAG, item);
+        extras.putInt(QUANTITY_TAG, quantity);
+
+        //Returning back to the main activity
+        resultIntent.putExtras(extras);
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
     }
 }
--- app/src/main/java/com/prasoon/shoppingcartv2/MainActivity.java
+++ app/src/main/java/com/prasoon/shoppingcartv2/MainActivity.java
@@ -1,12 +1,35 @@
 package com.prasoon.shoppingcartv2;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Using the Activity Result API
+    //For that, we have to first register a launcher, which will launch the required activity while expecting a result
+    //This launcher requires a contract, as well as a callback method to be called when the other activity finishes
+    //We are using an anonymous class for implementing the callback method
+    ActivityResultLauncher<Intent> activityLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {//Anonymous class
+                @Override
+                public void onActivityResult(ActivityResult result) {//Callback method
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent intent = result.getData();
+                        Bundle extras = intent.getExtras();
+
+                        //Log.d("DEBUG", "\nReturned :- \n 1. Item name = "+extras.getString(AddItem.ITEM_NAME_TAG)+ " \n 2. Quantity = "+extras.getInt(AddItem.QUANTITY_TAG));
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void addItem(View view){
         Intent intent = new Intent(this, AddItem.class);
-        startActivity(intent);
+        activityLauncher.launch(intent); //Using the recommended Activity Result API for launching another activity
     }
 
     /**
